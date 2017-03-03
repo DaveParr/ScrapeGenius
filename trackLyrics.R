ef519112aac557d42c84b50abd59a1dc01545c70
DaveRGP <DavidRGParr@gmail.com>
2017-03-03 17:57:45 +0000

    Removes libne breaks and all characters between [] attempts to make capture groups to fix run on capitalistation not working

--- trackLyrics.R
+++ trackLyrics.R
@@ -1,7 +1,14 @@
 library(rvest)
+library(stringr)
 
 # Input url for traCK
 track_url <- rvest::html("https://genius.com/Eminem-rap-god-lyrics")
 
 # Pipe the info into slightly messy text file
-track_url %>% html_node("lyrics") %>% html_text()
+# need to remove more identifiers
+track_url %>%
+  html_node("lyrics") %>%
+  html_text() %>%
+  str_replace_all("\\n", " ")%>% # change line break to space
+  str_replace_all("\\[.*?\\]", " ") %>% #remove anything in []
+  str_replace_all("([a-z])([A_Z])", "\\1 \\2") # fix wordRun on currently not working
